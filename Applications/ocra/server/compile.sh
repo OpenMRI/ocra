7f35d65713217b069c7146b6cc7748ad1077dfc2
Thomas Witzel <twitzel@mgh.harvard.edu>
2019-09-07 18:28:45 +0000

    remove the scp command to install the new code

--- Applications/ocra/server/compile.sh
+++ Applications/ocra/server/compile.sh
@@ -2,4 +2,4 @@
 # INP = $1
 # OUT = $2
 arm-linux-gnueabihf-gcc -static -O3 -march=armv7-a -mcpu=cortex-a9 -mtune=cortex-a9 -mfpu=neon -mfloat-abi=hard $1 -o $2 -lm
-scp $2 root@heleus.nmr.mgh.harvard.edu:/root/server/
+#scp $2 root@heleus.nmr.mgh.harvard.edu:/root/server/
