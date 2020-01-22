383596dd8c4cbe9aff25da604a8f3a12b80e9126
Thomas Witzel <twitzel@mgh.harvard.edu>
2020-01-22 18:56:24 +0000

    Updated the shim frame work to produce four cloned copies of the wabveforms at high sample rate

---
+++ Applications/shim_controller/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# INP = $1
+# OUT = $2
+arm-linux-gnueabihf-gcc -static -O3 -march=armv7-a -mcpu=cortex-a9 -mtune=cortex-a9 -mfpu=neon -mfloat-abi=hard $1 -o $2 -lm
+#scp $2 root@heleus.nmr.mgh.harvard.edu:/root/server/
