a905568a5de6ed3468e8f09858dfaeb722403212
Thomas Witzel <twitzel@mgh.harvard.edu>
2020-01-22 18:58:46 +0000

    More shim controller framework

--- HDL/projects/shim_controller/shim_dacs.tcl
+++ HDL/projects/shim_controller/shim_dacs.tcl
@@ -14,7 +14,7 @@ cell xilinx.com:ip:xlslice:1.0 slice_1 {
 # Create dac_spi_sequencer
 cell open-mri:user:axi_four_ltc2656_spi:1.0 spi_sequencer_0 {
   BRAM_DATA_WIDTH 32
-  BRAM_ADDR_WIDTH 11
+  BRAM_ADDR_WIDTH 16
   CONTINUOUS FALSE
   C_S_AXI_DATA_WIDTH 32
   C_S_AXI_ADDR_WIDTH 32
@@ -30,12 +30,12 @@ cell xilinx.com:ip:xlconcat:2.1 spiconcat_0 {
 
 # Make a constant setting the gradient DAC to 100 samples
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_0
-set_property -dict [list CONFIG.CONST_WIDTH {14} CONFIG.CONST_VAL {100}] [get_bd_cells xlconstant_0]
+set_property -dict [list CONFIG.CONST_WIDTH {16} CONFIG.CONST_VAL {100}] [get_bd_cells xlconstant_0]
 connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins spi_sequencer_0/cfg_data]
 
 # Make a constant setting the gradient DAC offset to 0
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_1
-set_property -dict [list CONFIG.CONST_WIDTH {14} CONFIG.CONST_VAL {0}] [get_bd_cells xlconstant_1]
+set_property -dict [list CONFIG.CONST_WIDTH {16} CONFIG.CONST_VAL {0}] [get_bd_cells xlconstant_1]
 connect_bd_net [get_bd_pins xlconstant_1/dout] [get_bd_pins spi_sequencer_0/current_offset]
 
 # Hookup the SPI stuff
