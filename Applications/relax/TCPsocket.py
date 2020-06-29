154763438c26ec00925ed6d040f1b2b28f668e90
Vlad Negnevitsky <vnegnev@users.noreply.github.com>
2020-06-29 23:20:01 +0200

    Remove empty folders, GUI and server from repo (will move to a separate repo)

--- Applications/relax/TCPsocket.py
+++
@@ -1,8 +0,0 @@
-# create global TCP socket
-
-from PyQt5.QtNetwork import QAbstractSocket, QTcpSocket
-
-socket = QTcpSocket()
-
-connected = QAbstractSocket.ConnectedState
-unconnected = QAbstractSocket.UnconnectedState
--- docs/index.md
+++
@@ -1,9 +0,0 @@
----
-title: Welcome
----
-
-## What is OCRA MRI?
-
-OCRA stands for Open source Console for Realtime Acquisitions.
-
-## Goals of this project
