e7652628f58d27d7634b1a34bd31be3403b0f074
Thomas Witzel <twitzel@mgh.harvard.edu>
2020-10-07 05:54:38 +0000

    Create a trigger core

--- HDL/cores/axi_trigger_core_v1_0/axi_trigger_core.v
+++ HDL/cores/axi_trigger_core_v1_0/axi_trigger_core.v
@@ -11,7 +11,10 @@ module axi_trigger_core #
 (
   // Serial Pins for the attenuator
   output wire 				    trigger_out,
-  input wire                                trigger_in, 				    
+  output wire 				    stretched_trigger_out, 
+  input wire 				    trigger_in,
+  input wire 				    aclk,
+  input wire                                aresetn, 				    
  // Do not modify the ports beyond this line
  
  // Global Clock Signal
@@ -104,6 +107,8 @@ module axi_trigger_core #
    reg [C_S_AXI_DATA_WIDTH-1:0]   slv_reg1;
    reg [C_S_AXI_DATA_WIDTH-1:0]   slv_reg2;
    reg [C_S_AXI_DATA_WIDTH-1:0]   slv_reg3;
+   reg [C_S_AXI_DATA_WIDTH-1:0]   slv_reg4;
+   reg [C_S_AXI_DATA_WIDTH-1:0]   slv_reg5;
    
    wire 			  slv_reg_rden;
    wire 			  slv_reg_wren;
@@ -370,6 +375,9 @@ module axi_trigger_core #
           4'h1   : reg_data_out <= slv_reg1;
           4'h2   : reg_data_out <= slv_reg2;
           4'h3   : reg_data_out <= slv_reg3;
+	  4'h4   : reg_data_out <= slv_reg4;
+	  4'h5   : reg_data_out <= slv_reg5;
+	  
           default : reg_data_out <= 0;
 	endcase
      end
@@ -394,8 +402,124 @@ module axi_trigger_core #
      end    
    
    // Add user logic here
-   wire aclk = S_AXI_ACLK;
-   wire aresetn = S_AXI_ARESETN;
 
+   
+   //wire aclk = S_AXI_ACLK;
+   //wire aresetn = S_AXI_ARESETN;
+   reg                  trigger_in_reg;
+   reg                  trigger_in_reg2;
+ 			
+   reg 	                trigger_out_reg;
+   reg                  trigger_lockout_reg; 			
+   reg [31:0] 		trigger_out_ctr;
+   reg [31:0] 		trigger_lockout_ctr;
+   reg                  stretched_trigger_out_reg;
+   reg [31:0] 		stretched_trigger_out_ctr;
+
+   reg [31:0] 		trigger_counter;
+ 		
+   assign trigger_out = trigger_out_reg;
+   assign stretched_trigger_out = stretched_trigger_out_reg;
+
+   always @(posedge aclk)
+     begin
+	if(~aresetn)
+	  begin
+	     trigger_in_reg <= 0;
+	     trigger_in_reg2 <= 0;
+	  end
+	else
+	  begin
+	     trigger_in_reg <= trigger_in;
+	     trigger_in_reg2 <= trigger_in_reg;
+	  end
+     end
+   
+   always @(posedge aclk)
+     begin
+	if (~aresetn)
+	  begin
+	     trigger_out_reg <= 0;
+	     trigger_out_ctr <= 32'd0;
+	     trigger_lockout_reg <= 0;
+	     trigger_lockout_ctr <= 32'd0;
+	     stretched_trigger_out_reg <= 0;
+	     stretched_trigger_out_ctr <= 32'd0;
+	     trigger_counter <= 32'd0;
+	     slv_reg4 <= 32'd0;
+	     slv_reg1 <= 32'd0;
+	     slv_reg5 <= 32'd0;
+	     
+	  end
+
+	else
+	  begin
+	     // Make sure trigger out reg is raised for 2 clock cycles
+	     if (trigger_out_reg == 0 && trigger_lockout_reg == 0)
+	       begin
+		  // inverted for now
+		  if (trigger_in_reg2 == 0)
+		    begin
+		       trigger_out_reg <= 1;
+		       trigger_out_ctr <= 32'd0;
+		       trigger_lockout_reg <= 1;
+		       trigger_lockout_ctr <= 32'd0;
+		       stretched_trigger_out_reg <= 1;
+		       stretched_trigger_out_ctr <= 0;
+		       trigger_counter <= trigger_counter + 32'd1;
+		       slv_reg4 <= slv_reg4 + 32'd1;
+		       slv_reg1 <= slv_reg1 + 32'd1;
+		       
+		    end
+	       end
+
+	     // for now set the lockout to 500us
+	     if (trigger_lockout_reg == 1)
+	       begin
+		  if (trigger_lockout_ctr == 32'd71429)
+		    begin
+		       trigger_lockout_reg <= 0;
+		       trigger_lockout_ctr <= 32'd0;
+		    end
+		  else
+		    begin
+		       trigger_lockout_ctr <= trigger_lockout_ctr + 32'd1;
+		    end
+	       end // if (trigger_lockout_reg == 1)
+
+	     // set the trigger out to 1 clock cycle
+	     if (trigger_out_reg == 1)
+	       begin
+		  
+		  //if (trigger_out_ctr == 32'd1)
+		  //  begin
+		  trigger_out_reg <= 0;
+		  slv_reg5  <= slv_reg5 + 32'd1;
+		  
+		  //     trigger_out_ctr <= 32'd0;
+		  //  end
+		  //else
+		  //  begin
+		  //     trigger_out_ctr <= trigger_out_ctr + 32'd1;
+		  //  end
+	       end // if (trigger_out_reg == 1)
+	     
+	
+	     // set the stretched trigger out to 10ms
+	     if (stretched_trigger_out_reg == 1)
+	       begin
+		  if (stretched_trigger_out_ctr == 32'd1428572)
+		    begin
+		       stretched_trigger_out_reg <= 0;
+		       stretched_trigger_out_ctr <= 0;
+		    end
+		  else
+		    begin
+		       stretched_trigger_out_ctr <=  stretched_trigger_out_ctr + 32'd1;
+		    end
+	       end // if (stretched_trigger_out_reg == 1)	     					   
+	  end // else: !if(~aresetn)
+	
+     end // always @ (posedge aclk)
    
 endmodule
