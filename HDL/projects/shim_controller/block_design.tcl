f65f4642b40b2f99b1244cac555aa68ec9866d46
Thomas Witzel <witzel.thomas@gmail.com>
2023-03-08 23:05:24 -0800

    adjusted the shim_controller to also use the new presets

--- HDL/projects/shim_controller/block_design.tcl
+++ HDL/projects/shim_controller/block_design.tcl
@@ -1,6 +1,11 @@
+global board_name
+global project_name
+
+set ps_preset boards/${board_name}/ps_${project_name}.xml
+
 # Create processing_system7
 cell xilinx.com:ip:processing_system7:5.5 ps_0 {
-  PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml
+  PCW_IMPORT_BOARD_PRESET $ps_preset
 } {
   M_AXI_GP0_ACLK ps_0/FCLK_CLK0
 }
