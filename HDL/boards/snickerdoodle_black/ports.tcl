1b2df688046f89b167ee08a28041d0fc52ce61d0
Thomas Witzel <witzel.thomas@gmail.com>
2023-03-28 13:03:26 -0700

    adding some port definitions for the snickerdoodle black

---
+++ HDL/boards/snickerdoodle_black/ports.tcl
@@ -0,0 +1,7 @@
+### Define the GPIO ports
+
+create_bd_port -dir IO -from 24 -to 0 gpio0_tri_io
+create_bd_port -dir IO -from 24 -to 0 gpio1_tri_io
+create_bd_port -dir IO -from 24 -to 0 gpio2_tri_io
+create_bd_port -dir IO -from 24 -to 0 gpio3_tri_io
+create_bd_port -dir IO -from 24 -to 0 gpio4_tri_io
