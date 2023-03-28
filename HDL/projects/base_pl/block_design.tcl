157404209a5b413a71d3ab655f1f0651cc674fa3
Thomas Witzel <witzel.thomas@gmail.com>
2023-03-28 12:19:21 -0700

    adding a base pl project

--- HDL/projects/base_pl/block_design.tcl
+++ HDL/projects/base_pl/block_design.tcl
@@ -6,8 +6,10 @@ set ps_preset boards/${board_name}/ps_${project_name}.xml
 # Create processing_system7
 cell xilinx.com:ip:processing_system7:5.5 ps_0 {
   PCW_IMPORT_BOARD_PRESET $ps_preset
+  PCW_USE_S_AXI_HP0 1
 } {
   M_AXI_GP0_ACLK ps_0/FCLK_CLK0
+  S_AXI_HP0_ACLK ps_0/FCLK_CLK0
 }
 
 # Create all required interconnections
@@ -17,5 +19,21 @@ apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
   Slave Disable
 } [get_bd_cells ps_0]
 
-# Create proc_sys_reset
-cell xilinx.com:ip:proc_sys_reset:5.0 rst_0
+# PLL
+
+# Create clk_wiz
+cell xilinx.com:ip:clk_wiz:6.0 pll_0 {
+  PRIMITIVE PLL
+  PRIM_IN_FREQ.VALUE_SRC USER
+  PRIM_IN_FREQ 125.0
+  PRIM_SOURCE Differential_clock_capable_pin
+  CLKOUT1_USED true
+  CLKOUT1_REQUESTED_OUT_FREQ 125.0
+  CLKOUT2_USED true
+  CLKOUT2_REQUESTED_OUT_FREQ 250.0
+  CLKOUT2_REQUESTED_PHASE -90.0
+  USE_RESET false
+} {
+  clk_in1_p adc_clk_p_i
+  clk_in1_n adc_clk_n_i
+}
