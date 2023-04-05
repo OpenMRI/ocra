e75757112fde177d4e451f0c16ed81a4c4893e7f
Thomas Witzel <witzel.thomas@gmail.com>
2023-04-05 13:00:56 -0700

    change the block design to address board specific changes

--- HDL/projects/base_pl/block_design.tcl
+++ HDL/projects/base_pl/block_design.tcl
@@ -19,9 +19,11 @@ apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
     Slave Disable
 } [get_bd_cells ps_0]
 
-# I don't think we need that for this project
-set_property CONFIG.PCW_USE_S_AXI_ACP {0} [get_bd_cells ps_0]
+# snickerdoodle_black specific stuff
+if {$board_name=="snickerdoodle_black"} {
+    # I don't think we need that for this project
+    set_property CONFIG.PCW_USE_S_AXI_ACP {0} [get_bd_cells ps_0]
 
-# We always have this though
-set_property CONFIG.PCW_USE_S_AXI_HP0 {1} [get_bd_cells ps_0]
-
+    # We always have this though
+    set_property CONFIG.PCW_USE_S_AXI_HP0 {1} [get_bd_cells ps_0]
+}
