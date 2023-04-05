f222e264ef1ced726585e01f15edbb8a02adb041
Thomas Witzel <witzel.thomas@gmail.com>
2023-04-05 12:57:57 -0700

    move the snickerdoodle boardfile into a subdir

--- HDL/scripts/Vivado_ocra_init.tcl
+++ HDL/scripts/Vivado_ocra_init.tcl
@@ -1,4 +1,4 @@
 
 # source this from your Vivado_init.tcl
 set ocra_dir $::env(OCRA_DIR)
-set_param board.repoPaths [list ${ocra_dir}/HDL/boards/snickerdoodle_black/1.0/ ]
+set_param board.repoPaths [list ${ocra_dir}/HDL/boards/snickerdoodle_black/brd/1.0/ ]
