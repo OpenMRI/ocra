a6c7342196ebb771b29ec7f3dd6053e4d93629bf
marcman86 <marcus-prier@gmx.de>
2023-04-27 17:11:58 +0200

    fix load fpga sh

--- utils/load_fpga_bit.sh
+++ utils/load_fpga_bit.sh
@@ -5,7 +5,7 @@ if [ -d /sys/kernel/config/device-tree/overlays/full ]; then
 fi
 
 echo 0 > /sys/class/fpga_manager/fpga0/flags
-cp ~/ocra_mri.bit.bin ~/ocra_mri.dtbo /lib/firmware/
+cp ~/stemlab_125_14_ocra_mri.bit.bin ~/stemlab_125_14_ocra_mri.dtbo /lib/firmware/
 
 mkdir /sys/kernel/config/device-tree/overlays/full
-echo -n "ocra_mri.dtbo" > /sys/kernel/config/device-tree/overlays/full/path
+echo -n "stemlab_125_14_ocra_mri.dtbo" > /sys/kernel/config/device-tree/overlays/full/path
