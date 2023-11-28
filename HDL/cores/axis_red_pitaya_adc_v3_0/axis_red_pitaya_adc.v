25d3192ebad440b0267a53be090d7682c5a8ddf6
Thomas Witzel <witzel.thomas@gmail.com>
2023-11-28 07:19:44 -0800

    fix the two's complement conversion and the switch logic

--- HDL/cores/axis_red_pitaya_adc_v3_0/axis_red_pitaya_adc.v
+++ HDL/cores/axis_red_pitaya_adc_v3_0/axis_red_pitaya_adc.v
@@ -27,6 +27,14 @@ module axis_red_pitaya_adc #
   reg  [ADC_DATA_WIDTH-1:0] int_dat_a_reg;
   reg  [ADC_DATA_WIDTH-1:0] int_dat_b_reg;
   reg [1:0] 		    int_channel_switch;
+  wire [ADC_DATA_WIDTH-1:0] channel_a;
+  wire [ADC_DATA_WIDTH-1:0] channel_b;
+   
+  initial begin
+     if (AXIS_TDATA_WIDTH/2 <= ADC_DATA_WIDTH) begin
+	$error("Assertion failed: this core does not support converting data without extending by at least one bit!");
+     end
+  end
    
   always @(posedge aclk)
   begin
@@ -38,10 +46,25 @@ module axis_red_pitaya_adc #
   assign adc_csn = 1'b1;
 
   assign m_axis_tvalid = 1'b1;
- 
-  assign m_axis_tdata = (int_channel_switch == 2'b01) ? {{(SINGLE_PADDING_WIDTH+1){int_dat_a_reg[ADC_DATA_WIDTH-1]}}, ~int_dat_a_reg[ADC_DATA_WIDTH-2:0]} :
- 			(int_channel_switch == 2'b10) ? {{(SINGLE_PADDING_WIDTH+1){int_dat_b_reg[ADC_DATA_WIDTH-1]}}, ~int_dat_b_reg[ADC_DATA_WIDTH-2:0]} :
-			                                {{(PADDING_WIDTH+1){int_dat_b_reg[ADC_DATA_WIDTH-1]}}, ~int_dat_b_reg[ADC_DATA_WIDTH-2:0],
-                                                        {(PADDING_WIDTH+1){int_dat_a_reg[ADC_DATA_WIDTH-1]}}, ~int_dat_a_reg[ADC_DATA_WIDTH-2:0]};
+
+  always @(*) begin
+     // generate the 2's complement correctly
+     assign channel_a = {~int_dat_a_reg[WIDTH-1], int_dat_a_reg[WIDTH-2:0]};
+     assign channel_b = {~int_dat_b_reg[WIDTH-1], int_dat_b_reg[WIDTH-2:0]};
+
+     // now construct the output
+     case (int_channel_switch)
+       2'b00:
+	m_axis_tdata = {{AXIS_TDATA_WIDTH/2{1'b0}}, -{{PADDING_WIDTH{channel_a[WIDTH-1]}},channel_a}};
+       2'b01:
+	m_axis_tdata = {{AXIS_TDATA_WIDTH/2{1'b0}}, -{{PADDING_WIDTH{channel_b[WIDTH-1]}},channel_b}};
+       2'b10:
+	// IQ mode
+	m_axis_tdata = {{AXIS_TDATA_WIDTH/2{1'b0}}, -{{PADDING_WIDTH{channel_a[WIDTH-1]}},channel_a},{AXIS_TDATA_WIDTH/2{1'b0}}, -{{PADDING_WIDTH{channel_b[WIDTH-1]}},channel_b}};
+       2'b11:
+	 // future loopback mode
+	 m_axis_tdata = {AXIS_TDATA_WIDTH{1'b0}};
+     endcase
+  end
 
 endmodule
