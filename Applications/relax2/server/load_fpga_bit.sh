7054b199f6074888f8fa30b2910564652f5423bf
marcman86 <marcus-prier@gmx.de>
2023-12-03 10:40:36 +0100

    add clean files

---
+++ Applications/relax2/server/load_fpga_bit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d /sys/kernel/config/device-tree/overlays/full ]; then
+    rmdir /sys/kernel/config/device-tree/overlays/full
+fi
+
+echo 0 > /sys/class/fpga_manager/fpga0/flags
+cp ~/stemlab_125_14_ocra_mri.bit.bin ~/stemlab_125_14_ocra_mri.dtbo /lib/firmware/
+
+mkdir /sys/kernel/config/device-tree/overlays/full
+echo -n "stemlab_125_14_ocra_mri.dtbo" > /sys/kernel/config/device-tree/overlays/full/path
